18cbebb2aad3a59bfd0179f35639e9c3def77134
siddhantpardhi <siddhantpardhi10@gmail.com>
2025-05-31 17:06:29 +0530

    some changes in redis configuration

--- server/src/utils/redisClient.js
+++ server/src/utils/redisClient.js
@@ -1,9 +1,28 @@
-import { createClient } from 'redis';
+import { createClient } from 'redis'
+import dotenv from "dotenv"
 
-const redisClient = createClient();
+dotenv.config()
+
+const isLocal = process.env.REDIS_URL.includes("localhost")
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+    ...(isLocal ? {} : {
+        socket: {
+          tls: true,
+          rejectUnauthorized: false,
+        }
+      })
+  })
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
-await redisClient.connect();
+await redisClient.connect()
+.then(() => {
+    console.log("Redis Connected")
+})
+.catch((err) => {
+    console.error("Redis Error: ", err)
+})
 
 export default redisClient;
